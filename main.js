6c3a29b9b2b9877e6dee91029d9da8b9a3742611
TraceOfLight <rickyjun45@gmail.com>
2022-02-13 21:46:02 +0900

    JS : Sticky Navbar
    Sticky and transparent navbar

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+"Use Strict";
 
+const nav = document.getElementsByClassName("navbar__all");
+
+window.onscroll = function sticky() {
+  if (window.scrollY > nav[0].scrollTop) {
+    nav[0].classList.add("nav");
+  } else {
+    nav[0].classList.remove("nav");
+  }
+};
