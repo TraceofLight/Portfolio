b95f8491d199d9461c0317dbac4a8a58bb26c5d1
TraceOfLight <rickyjun45@gmail.com>
2022-02-16 19:32:58 +0900

    JS : Popup Menu (1)

--- main.js
+++ main.js
@@ -3,9 +3,26 @@
 const nav = document.getElementsByClassName("navbar__all");
 
 window.onscroll = function sticky() {
-  if (window.scrollY > nav[0].scrollTop) {
-    nav[0].classList.add("nav");
+  if (window.scrollY > nav.scrollTop) {
+    nav.classList.add("nav");
   } else {
-    nav[0].classList.remove("nav");
+    nav.classList.remove("nav");
   }
 };
+
+const media = matchMedia("screen and (max-width: 840px)")
+const nav_toggle = document.getElementsByClassName("navbar__toggle-btn");
+const nav_menu = document.getElementsByClassName("navbar__menu");
+const nav_display = media.nav_menu.style.display;
+
+nav_toggle.addEventListener("click", display());
+
+function display() {
+  if(media.nav_menu.style.display == 'none') {
+    media.nav_menu.style.display = 'flex',
+    media.nav_menu.classList.add("nav__menu_onclick");
+  } else {
+    media.nav_menu.style.display = 'none',
+    media.nav_menu.classList.remove("nav__menu_onclick");
+  }
+}
